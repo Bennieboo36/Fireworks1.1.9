6ee3b9ecd4dbe4224165d1a25ea4281064d38b9f
Bennieboo36 <bennittoburritojoe@gmail.com>
2026-02-10 11:30:29 -0800

    Add files via upload

--- projectflowfield.py
+++ projectflowfield.py
@@ -1,6 +1,8 @@
 # import turtle and math
 import turtle
 import math
+import time
+
 
 t = turtle.Turtle()
 t.speed(0)
@@ -76,21 +78,21 @@ for i in range(360):
     
     # mathematical drawing
     t.penup()
-    t.goto(-275,170)
+    t.goto(-400,170)
     dist = math.sin(math.radians(i * 5)) * 80
     t.pendown()
     t.forward(dist)
     t.left(59)
     # return to center of firework without drawing
     t.penup()
-    t.goto(-275,170)
+    t.goto(-400,170)
     t.pendown()
 
 # draw grass
 t.penup()
 t.goto(-600,-100)
 t.pendown()
-t.fillcolor("green")
+t.fillcolor("darkgreen")
 t.begin_fill()
 for count in range(2):
     t.forward(1200)
@@ -99,4 +101,78 @@ for count in range(2):
     t.right(90)
 t.end_fill()
 
-turtle.done()
+# add couple on grass
+wn = turtle.Screen()
+people_image = "People sitting.gif"
+wn.addshape(people_image)
+people = turtle.Turtle()
+people.shape(people_image)
+people.penup()
+people.goto(0,-100)
+people.pendown()
+
+# draw infinite loop of fuses animation
+# make new turtle so fireworks and grass don't clear when fuses clear
+fuse = turtle.Turtle()
+fuse.hideturtle()
+fuse.speed(0)
+
+while True: 
+    # firework and fuse 1
+    fuse.penup()
+    fuse.goto(-200, -100)
+    fuse.setheading(90)
+    for d in range(-100, 275, 15):
+        fuse.pencolor("purple" if (d//15)%2==0 else "white")
+        fuse.pendown()
+        fuse.forward(10)
+        fuse.penup()
+        fuse.forward(5)
+        wn.update()
+        time.sleep(0.01)
+    fuse.clear() 
+
+    # firework fuse 2
+    fuse.penup()
+    fuse.goto(25, -100)
+    fuse.setheading(90)
+    for d in range(-100, 195, 15):
+        fuse.pencolor("lightgreen" if (d//15)%2==0 else "white")
+        fuse.pendown()
+        fuse.forward(10)
+        fuse.penup()
+        fuse.forward(5)
+        wn.update()
+        time.sleep(0.01)
+    fuse.clear()
+
+    # firework fuse 3
+    fuse.penup()
+    fuse.goto(300, -100)
+    fuse.setheading(90)
+    for d in range(-100, 250, 15):
+        fuse.pencolor("pink" if (d//15)%2==0 else "white")
+        fuse.pendown()
+        fuse.forward(10)
+        fuse.penup()
+        fuse.forward(5)
+        wn.update()
+        time.sleep(0.01)
+    fuse.clear()
+
+    # firework fuse 4
+    fuse.penup()
+    fuse.goto(-400, -100)
+    fuse.setheading(90)
+    for d in range(-100, 170, 15):
+        fuse.pencolor("yellow" if (d//15)%2==0 else "white")
+        fuse.pendown()
+        fuse.forward(10)
+        fuse.penup()
+        fuse.forward(5)
+        wn.update()
+        time.sleep(0.01)
+    fuse.clear()
+
+wn = turtle.Screen()
+wn.mainloop()
